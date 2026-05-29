be5381c33ae64058c5f2ed6678a9c1613009a17e
vivek <vivek.121@outlook.com>
2026-05-29 13:05:48 +0530

    this commit from asia time guy

---
+++ src/gitdemo/RestApiTest.java
@@ -0,0 +1,18 @@
+package gitdemo;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void postJira() {
+		System.out.println("post gira");
+		System.out.println("post gira2");
+		System.out.println("post gira3");
+		
+	}
+
+	public void deleteTwitter() {
+		System.out.println("delete twitter");
+	}
+}
